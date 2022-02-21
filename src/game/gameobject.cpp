85ccaf8d46228a513a95806340ee1ddaf05ae8c9
Jan Ráček <racek.janek@gmail.com>
2022-02-21 13:35:24 +0100

    Added component class and removed binary from repo

--- src/game/gameobject.cpp
+++ src/game/gameobject.cpp
@@ -3,3 +3,9 @@
 GameObject::GameObject(glm::vec3 position) {
 	_position = position;
 }
+
+GameObject::addComponent(Component& component) {
+	_components.push_back(component);
+:wq!
+
+
--- src/game/gameobject.h
+++ src/game/gameobject.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <vector>
+
 #include <glm/glm.hpp>
+
+#include "component.h"
 
 class GameObject {
 	public:
@@ -8,6 +12,14 @@ class GameObject {
 		
 		glm::vec3 getPosition();
 		void setPosition(glm::vec3 position);
+
+		std::vector<Component> getComponents();
+		Component getComponent(uint id);
+		void addComponent(Component& component);
+		void removeComponent(uint componentID);
+	
 	private:
 		glm::vec3 _position;
+		std::vector<std::reference_wrapper<Component>> _components;
+
 };
