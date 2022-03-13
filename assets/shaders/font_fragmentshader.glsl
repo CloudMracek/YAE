accdc9d3505ba95005a0a6a097021a1fc01b098f
Jan Ráček <racek.janek@gmail.com>
2022-03-13 08:43:29 +0100

    Fixed broken font shader

--- assets/shaders/font_fragmentshader.glsl
+++ assets/shaders/font_fragmentshader.glsl
@@ -5,8 +5,6 @@ out vec4 color;
 layout (binding = 0) uniform sampler2D text;
 layout (location = 6) uniform vec3 textColor;
 
-uniform vec3 textColor;
-
 void main()
 {    
 	color = vec4(textColor, 1.0) * texture(text, TexCoords).r;
